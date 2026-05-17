3264fa179635c825a0cfd1efc121070a62e588d1
Plant Disease Detection <plant@detection.com>
2026-05-17 17:20:44 +0500

    Change to Config.py

--- config.py
+++ config.py
@@ -1,13 +1,17 @@
 import os
 
-BASE_DIR    = r"D:\AI Project\plant-disease-detection"
+# Dynamic base directory mapping (works on both Windows and Railway Linux)
+BASE_DIR    = os.path.dirname(os.path.abspath(__file__))
+
 DATASET_DIR = os.path.join(BASE_DIR, "dataset")
 TRAIN_DIR   = os.path.join(DATASET_DIR, "train")
 VAL_DIR     = os.path.join(DATASET_DIR, "val")
 TEST_DIR    = os.path.join(DATASET_DIR, "test")
+
 MODEL_DIR   = os.path.join(BASE_DIR, "model")
 MODEL_PATH  = os.path.join(MODEL_DIR, "plant_model.keras")
 CLASS_JSON  = os.path.join(MODEL_DIR, "class_names.json")
+
 OUTPUT_DIR  = os.path.join(BASE_DIR, "outputs")
 PLOTS_DIR   = os.path.join(OUTPUT_DIR, "plots")
 CM_DIR      = os.path.join(OUTPUT_DIR, "confusion_matrix")
